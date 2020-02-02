e25a8d5247b03d37a70ade931487d9b8a4d4bd34
Uditha Atukorala <ua@nuked.zone>
2020-02-02 20:37:07 +0000

    [zsh] add google.zsh

---
+++ zshrc.d/google.zsh
@@ -0,0 +1,8 @@
+# google.zsh
+
+# gcloud sdk
+if [ -d "${HOME}/root/opt/google-cloud-sdk" ] ; then
+	source "${HOME}/root/opt/google-cloud-sdk/path.zsh.inc"
+	source "${HOME}/root/opt/google-cloud-sdk/completion.zsh.inc"
+fi
+
