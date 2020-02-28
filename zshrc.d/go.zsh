21c869f48020f22a1b64cdb3af95267a2f703708
Uditha Atukorala <ua@nuked.zone>
2020-02-28 17:01:10 +0000

    [zsh] updated go.zsh

--- zshrc.d/go.zsh
+++ zshrc.d/go.zsh
@@ -3,7 +3,7 @@
 if [ -d "${HOME}/root/opt/go/bin" ] ; then
 	export GOROOT="${HOME}/root/opt/go"
 	export GOPATH="${HOME}/workbench/go"
-	export GOPRIVATE="gitlab.com/thirdfort.io"
+	export GOPRIVATE="github.com/thirdfort,gitlab.com/thirdfort.io"
 
 	PATH="${GOROOT}/bin:${GOPATH}/bin${PATH:+:${PATH}}"
 fi
