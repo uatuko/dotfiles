1517305c5eca429cbf59ced3b4cf678fad03f95b
Uditha Atukorala <ua@nuked.zone>
2024-09-26 09:57:01 +0100

    [zsh] tidy-up

--- zshrc.d/go.zsh
+++ zshrc.d/go.zsh
@@ -3,7 +3,6 @@
 if [ -d "${HOME}/root/opt/go/bin" ] ; then
 	export GOROOT="${HOME}/root/opt/go"
 	export GOPATH="${HOME}/workbench/go"
-	export GOPRIVATE="github.com/thirdfort,gitlab.com/thirdfort.io"
 
 	PATH="${GOROOT}/bin:${GOPATH}/bin${PATH:+:${PATH}}"
 fi
