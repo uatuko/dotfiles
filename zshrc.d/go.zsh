0e222cfee216cdd3e7fc3a9a82937b6fc4727ae4
Uditha Atukorala <ua@nuked.zone>
2024-10-27 10:54:39 +0000

    [zsh] tidy-up

--- zshrc.d/go.zsh
+++ zshrc.d/go.zsh
@@ -2,7 +2,7 @@
 
 if [ -d "${HOME}/root/opt/go/bin" ] ; then
 	export GOROOT="${HOME}/root/opt/go"
-	export GOPATH="${HOME}/workbench/go"
+	export GOPATH="${HOME}/wb/go"
 
 	PATH="${GOROOT}/bin:${GOPATH}/bin${PATH:+:${PATH}}"
 fi
